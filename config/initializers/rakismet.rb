651c368b67163fcfa935b23bea4382e1417723b0
Michael Patricios <michael.patricios@gmail.com>
2011-07-30 20:12:36 +0100

    Fix incorrect config key

--- config/initializers/rakismet.rb
+++ config/initializers/rakismet.rb
@@ -1,4 +1,4 @@
-if defined? APP_CONFIG and APP_CONFIG.akismet_enabled? and !APP_CONFIG.akismet_api_key.blank?
+if defined? APP_CONFIG and APP_CONFIG.akismet_enabled and !APP_CONFIG.akismet_api_key.blank?
   Rakismet::KEY = APP_CONFIG.akismet_api_key
   Rakismet::URL = APP_CONFIG.domain
   Rakismet::HOST = 'rest.akismet.com'
