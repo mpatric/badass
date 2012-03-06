3de5d37bfca54cbc48f91e0c8dbdb4641cefa53a
Michael Patricios <michael.patricios@gmail.com>
2012-03-06 09:24:54 +0000

    More concise code

--- app/controllers/badass_controller.rb
+++ app/controllers/badass_controller.rb
@@ -11,13 +11,11 @@ class BadassController < ActionController::Base
     
   private
     def current_user_session
-      return @current_user_session if defined?(@current_user_session)
-      @current_user_session = UserSession.find
+      @current_user_session ||= UserSession.find
     end
     
     def current_user
-      return @current_user if defined?(@current_user)
-      @current_user = current_user_session && current_user_session.record
+      @current_user ||= current_user_session && current_user_session.record
     end
     
     def require_user
