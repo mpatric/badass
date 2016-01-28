a495e8e098493ff2e236ce96e1d13e558c0ba888
Michael Patricios <michael.patricios@gmail.com>
2016-01-28 20:05:54 +0000

    Fix setting rakismet config

--- config/initializers/rakismet.rb
+++ config/initializers/rakismet.rb
@@ -1,5 +1,5 @@
 if defined? APP_CONFIG and APP_CONFIG.akismet_enabled and !APP_CONFIG.akismet_api_key.blank?
-  Rakismet::KEY = APP_CONFIG.akismet_api_key
-  Rakismet::URL = APP_CONFIG.domain
-  Rakismet::HOST = 'rest.akismet.com'
+  rakismet_config = Rails.application.config.rakismet
+  rakismet_config.key = APP_CONFIG.akismet_api_key
+  rakismet_config.url = APP_CONFIG.domain
 end
